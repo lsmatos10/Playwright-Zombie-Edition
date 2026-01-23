abbd0acaa8f8a955d9179874fdc0bf71d2963eb9
Leonardo Matos <devleomatos@gmail.com>
2026-01-23 10:09:55 -0300

    realizar checkpoint no modal e usando elementos

--- projects/zombieplus/playwright.config.js
+++ projects/zombieplus/playwright.config.js
@@ -39,7 +39,8 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
+  
+/*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -49,7 +50,7 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
+/*
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
--- projects/zombieplus/tests/leads.spec.js
+++ projects/zombieplus/tests/leads.spec.js
@@ -4,8 +4,17 @@ import { test, expect } from '@playwright/test';
 test('deve cadastrar um lead na fila de espera', async ({ page }) => {
   await page.goto('http://localhost:3000/');
 
-  await page.getByRole('button', {name: 'Aperte o play' }).click()
-  await page.waitForTimeout(1000)
+  await page.getByRole('button', {name: /Aperte o play/ }).click()
 
+  await expect(
+    page.getByTestId('modal').getByRole('heading')
+  ).toHaveText('Fila de espera') // Realizar checkpoint no modal
+  
+
+  //await page.locator('#name').fill('leo')
+  //await page.locator('input[placeholder="Seu nome completo"]').fill('leo') //usando pelo placehoplder
+  await page.getByPlaceholder('Seu nome completo').fill('leo teste') //usando pelo placehoplder
+  await page.locator('input[name=email]').fill('leoteste@yahoo.com') // Procurar pelo name
+  await page.waitForTimeout(10000)
 
 });
